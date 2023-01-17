571d437498002a94555383359c1a14f7f98894f0
Yvan Sugira <phoenixnavyn7@gmail.com>
2023-01-17 15:32:52 +0200

    [fix] Config

--- backend/src/main/java/com/tenant/models/Car.java
+++ backend/src/main/java/com/tenant/models/Car.java
@@ -17,6 +17,7 @@ public class Car {
     private String plateNumber;
 
     @Column
+    @
     private String owner;
 
     @Column
@@ -41,4 +42,84 @@ public class Car {
 
     @Column
     private String Currency;
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(String borrower) {
+        this.borrower = borrower;
+    }
+
+    public bool getIsBooked() {
+        return isBooked;
+    }
+
+    public void setIsBooked(bool isBooked) {
+        this.isBooked = isBooked;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Date getAdded() {
+        return added;
+    }
+
+    public void setAdded(Date added) {
+        this.added = added;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return Currency;
+    }
+
+    public void setCurrency(String currency) {
+        Currency = currency;
+    }
 }
