bb1348abd4fa33fb1d4de032fb6c17ba2ff25b90
mugishap <precieuxmugisha@gmail.com>
2023-01-17 08:23:24 +0200

    [fix]: COnfig

--- backend/src/main/java/com/tenant/Main.java
+++ backend/src/main/java/com/tenant/Main.java
@@ -1,5 +1,10 @@
 package com.tenant;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
 
